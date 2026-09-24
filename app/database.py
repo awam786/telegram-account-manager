dbbfb6b41345eab6481060bb131266e26d324996
awam786 <awamwaled@gmail.com>
2026-09-24 11:44:45 +0500

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from .config import DATABASE_URL
+
+
+engine = create_async_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
